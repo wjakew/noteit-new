0ed6f57a86ec0da89f25e294613a2bf399a0a38a
Jakub Wawak <34348257+wjakew@users.noreply.github.com>
2023-04-12 15:00:30 +0200

    noteit-120423REV1

---
+++ src/main/java/com/jakubwawak/website/website_content_objects/note_components/NoteListGrid.java
@@ -0,0 +1,24 @@
+/**
+ * by Jakub Wawak
+ * kubawawak@gmail.com
+ * all rights reserved
+ */
+package com.jakubwawak.website.website_content_objects.note_components;
+
+import com.jakubwawak.support_objects.Note;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class NoteListGrid {
+
+    Grid<Note> grid;
+    TextField grid_searchbox_field;
+
+    /**
+     * Constructor
+     * @param noteit_vault_id
+     */
+    public NoteListGrid(int noteit_vault_id){
+
+    }
+}
